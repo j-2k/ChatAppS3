be62afe5f93a634dfd0313f3117e0459f253f387
turmoil2k <juma1112009@hotmail.com>
2021-03-04 09:50:38 +0400

    Pushing Binary Formatter class with working Client Class

--- ChatApp/BinaryFormatterClass.cs
+++ ChatApp/BinaryFormatterClass.cs
@@ -11,25 +11,25 @@ namespace ChatApp
     class BinaryFormatterClass
     {
 
-        public static byte[] ObjectToByteArray(Object obj)
+        public static byte[] ObjectToByteArray(Object obj)  // OBJECT TO BYTE BF
         {
-            BinaryFormatter bf = new BinaryFormatter();
-            using (MemoryStream ms = new MemoryStream())
+            BinaryFormatter bf = new BinaryFormatter();     //Creating a new binary formatter
+            using (MemoryStream ms = new MemoryStream())    //making a memory stream //using responsible for memory cleanup
             {
-                bf.Serialize(ms, obj);
-                return ms.ToArray();
+                bf.Serialize(ms, obj);                      //serialize the stream with the object we take in
+                return ms.ToArray();                        //return the serialized stream
             }
         }
 
-        public static Object ByteArrayToObject(Byte[] byteArray)
+        public static Object ByteArrayToObject(Byte[] byteArray)    // BYTE TO OBJECT BF
         {
-            using(MemoryStream ms = new MemoryStream())
+            using(MemoryStream ms = new MemoryStream())             //a new mem stream
             {
-                BinaryFormatter bf = new BinaryFormatter();
-                ms.Write(byteArray, 0, byteArray.Length);
-                ms.Seek(0, SeekOrigin.Begin);
-                Object obj = bf.Deserialize(ms);
-                return obj;
+                BinaryFormatter bf = new BinaryFormatter();         //new bf & begin undoing the serialization (deserialize)
+                ms.Write(byteArray, 0, byteArray.Length);           //write the entire byte array to memory with offset 0 to the size of the byte array
+                ms.Seek(0, SeekOrigin.Begin);                       //setting the START of the memory stream we put 0 becaues we dont want an offset & we make it start at the start of the MS
+                Object obj = bf.Deserialize(ms);                    //then finally the object will be equal to the deserialized MS (using BF to deserialize)
+                return obj;                                         //return deserialized obj
             }
         }
 
